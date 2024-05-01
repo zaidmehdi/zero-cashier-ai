da8a6eda11f8f8695a6c854711407ea3d3b3c8e5
zaidmehdi <mehdizaid02@gmail.com>
2024-05-01 17:25:05 +0200

    ignore pycache

--- src/main.py
+++ src/main.py
@@ -1,28 +1,9 @@
-import cv2
+from object_detection import ObjectDetection
 
 
 def main():
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        ret, frame = cap.read()
-        frame = cv2.flip(frame, 1)
-        
-        if not ret:
-            break
-
-        cv2.imshow('Camera Feed', frame)
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-    from ultralytics import YOLO
-    model = YOLO("yolov8n.pt")
-    results = model("src/test.png")
-    print(results) 
+    detector = ObjectDetection(capture_index=0)
+    detector()
 
 
 if __name__ == "__main__":
